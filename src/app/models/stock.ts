5136d4a00852265cd99ffba2deae9443b055bc4d
rsolervi <96911937+rsolervi@users.noreply.github.com>
2022-09-19 22:43:51 +0200

    1 file modified

--- src/app/models/stock.ts
+++ src/app/models/stock.ts
@@ -1,11 +1,11 @@
 export interface Stock {
   name?: string;
   symbol?: string;
-  currentPrice: number;
-  change: number;
-  percent: number;
-  highPrice: number;
-  lowPrice: number;
-  openPrice: number;
-  previousClosePrice: number;
+  currentPrice?: number;
+  change?: number;
+  percent?: number;
+  highPrice?: number;
+  lowPrice?: number;
+  openPrice?: number;
+  previousClosePrice?: number;
 }
